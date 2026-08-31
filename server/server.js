c25b494a32dca5576dc31f5516a4c4e78e1ba4c7
Suraj <mahatosuraj6224@gmail.com>
2026-08-31 21:41:26 +0530

    Connect to MongoDB on server startup
    Added MongoDB connection setup to server initialization.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,14 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
